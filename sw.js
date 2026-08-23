6d1ceb1feb898f3792221a336b67b35cc3fcb0b3
Adwithya Magow <avmagow@gmail.com>
2026-08-23 22:32:48 +0800

    Refresh PWA assets before cached fallback

--- sw.js
+++ sw.js
@@ -1,4 +1,11 @@
-const CACHE = "div-it-v2";
+const CACHE = "div-it-v3";
 const FILES = ["./", "./index.html", "./style.css", "./app.js", "./ledger.js", "./manifest.webmanifest"];
-self.addEventListener("install", (event) => event.waitUntil(caches.open(CACHE).then((cache) => cache.addAll(FILES))));
-self.addEventListener("fetch", (event) => event.respondWith(caches.match(event.request).then((hit) => hit || fetch(event.request))));
+self.addEventListener("install", (event) => event.waitUntil(caches.open(CACHE).then((cache) => cache.addAll(FILES)).then(() => self.skipWaiting())));
+self.addEventListener("activate", (event) => event.waitUntil(clients.claim()));
+self.addEventListener("fetch", (event) => {
+  if (event.request.method !== "GET") return;
+  event.respondWith(fetch(event.request).then((response) => {
+    caches.open(CACHE).then((cache) => cache.put(event.request, response.clone()));
+    return response;
+  }).catch(() => caches.match(event.request)));
+});
